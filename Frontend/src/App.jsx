d0d3e13cd9eec5ce651e8ffa620e11601c439bd8
Jaco Botha <jacobotha206@gmail.com>
2026-02-03 09:00:50 +0200

    Revert "feat: routing created (#22)" (#23)
    This reverts commit 534fd23d340ef642582d0eb08b3af4ff45fa0a6d.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,45 +1,35 @@
-
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import {BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
-
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <>
-        <nav className='flex flex-row justify-center gap-7 top-0'>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/about"}>About</Link>
-          <Link to={"/services"}>Services</Link>
-        </nav>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/about"
-            element={<About />}
-          />
-          <Route path='/services' element={<Services />} />
-          <Route
-            path="*"
-            element={<h2>404: Page doesnt exsist</h2>}
-          />
-        </Routes>
-        {/* Tested Tailwind works in app  */}
-
-      </>
-    </BrowserRouter>
-  );
+    <>
+      <div className='flex flex-row'> {/* Tested Tailwind works in app  */}
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
