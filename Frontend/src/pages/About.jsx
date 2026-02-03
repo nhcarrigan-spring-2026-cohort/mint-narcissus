534fd23d340ef642582d0eb08b3af4ff45fa0a6d
Jaco Botha <jacobotha206@gmail.com>
2026-02-03 08:43:02 +0200

    feat: routing created (#22)

---
+++ Frontend/src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <section>
+      <div>
+        <h2>About Page</h2>
+      </div>
+    </section>
+  );
+}
---
+++ Frontend/src/pages/Services.jsx
@@ -0,0 +1,5 @@
+
+
+export default function Services() {
+    return (<section><div><h2>Service page</h2></div></section>)
+}
