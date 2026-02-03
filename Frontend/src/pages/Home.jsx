ca302ad04be891d6e5472f59fa4d4ecca891b0ed
JohannesBotha <jacobotha206@gmail.com>
2026-02-03 08:33:58 +0200

    feat: routing created

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Home () {
+    return (
+      <section>
+        <div>
+          <h1 className="p-4">You Got The Skill...Lets get you that Job!</h1>
+        </div>
+      </section>
+    );
+}
