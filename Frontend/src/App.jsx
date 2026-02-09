7dd4c2fbaca02a9e79b80f40d1d384286fec64f3
JohannesBotha <jacobotha206@gmail.com>
2026-02-09 15:09:24 +0200

    Formatting improved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,22 @@
-import "./App.css";
-import Container from "./components/AppLayout";
+import './App.css';
+import Container from './components/AppLayout';
 
 /* Page Components */
-import Browse from "./pages/Browse";
-import Saved from "./pages/Saved";
-import Messages from "./pages/Messages";
-import AppLayout from "./components/AppLayout";
-
+import Browse from './pages/Browse';
+import Saved from './pages/Saved';
+import Messages from './pages/Messages';
+import AppLayout from './components/AppLayout';
 
 /* Array passed to Container.jsx to initiate Links */
 const navItems = [
-  { label: "Browse", path: "/browse", element: <Browse /> },
-  { label: "Saved", path: "/saved", element: <Saved /> },
-  { label: "Messages", path: "/messages", element: <Messages /> },
+  { label: 'Browse', path: '/browse', element: <Browse /> },
+  { label: 'Saved', path: '/saved', element: <Saved /> },
+  { label: 'Messages', path: '/messages', element: <Messages /> },
 ];
 
 function App() {
   /* Maybe add a footer section aswell later */
-  return <AppLayout  navItems={navItems}></AppLayout>;
+  return <AppLayout navItems={navItems}></AppLayout>;
 }
 
 export default App;
