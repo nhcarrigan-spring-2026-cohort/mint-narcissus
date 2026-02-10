2f02c769aefe7fd27ba70133a6783fe24150630a
JohannesBotha <jacobotha206@gmail.com>
2026-02-10 12:08:15 +0200

    Created /components/OutfitCard.jsx Created /components/OutfitGallery.jsx

---
+++ Frontend/src/components/OutfitGallery.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import ProductCard from "./OutfitCard";
+
+export default function OutfitGallery() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleFavorite = () => {
+    setIsFavorite((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <ProductCard
+        title="Classic Navy Suit"
+        imgSrc={
+          "https://images.unsplash.com/photo-1507679799987-c73779587ccf?w=600&amp;h=800&amp;fit=crop"
+        }
+        isFavorite={isFavorite}
+        onFavoriteClick={handleFavorite}
+        status={"Available"}
+        tags={["Formal", "Corporate", "Finance"]}
+        fitInfo={"5'9-6'0, Average build"}
+        quote={"Coolest ever"}
+      />
+    </div>
+  );
+}
