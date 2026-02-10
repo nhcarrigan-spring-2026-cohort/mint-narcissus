b8bb3205fde91018e29f3e88b58cbf6b3c263683
JohannesBotha <jacobotha206@gmail.com>
2026-02-10 10:12:09 +0200

    Installed shadcn with components. Created Card.jsx component

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+import path from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,7 +17,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
