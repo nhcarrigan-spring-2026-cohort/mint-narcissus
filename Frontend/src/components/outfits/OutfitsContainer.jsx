4778e3f2b10e66a5e42f6dbe81f33af423a79db8
JohannesBotha <jacobotha206@gmail.com>
2026-02-18 09:35:01 +0200

    first Commit

--- Frontend/src/components/outfits/OutfitsContainer.jsx
+++ Frontend/src/components/outfits/OutfitsContainer.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
+import OutfitCard from './OutfitCard';
+import OutfitDetailModal from './OutfitDetailModal';
+import {MockOutfits} from './MockOutfits';
 
-const OutfitsContainer = () => {
-  return <div>Outfits Container</div>;
-};
 
-export default OutfitsContainer;
+export default function OutfitsContainer() {
+  const [selectedOutfit, setSelectedOutfit] = useState(null);
+
+  const [favorites, setFavorites] = useState({});
+
+  
+
+  const handleFavorite = (outfitId) => {
+    setFavorites((prev) => ({ ...prev, [outfitId]: !prev[outfitId] }));
+  };
+
+  const handleCardClick = (outfit) => setSelectedOutfit(outfit);
+
+  const handleCloseModal = () => setSelectedOutfit(null);
+
+
+  return (
+    <>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6'>
+        {MockOutfits.map((outfit) => (
+          <div
+            className='cursor-pointer'
+            key={outfit.id}
+            onClick={() => handleCardClick(outfit)}
+          >
+            <OutfitCard
+              title={outfit.title}
+              imgSrc={outfit.imgSrc}
+              status={outfit.status}
+              tags={outfit.tags}
+              category={outfit.category}
+              fabric={outfit.fabric}
+              fitInfo={outfit.fitInfo}
+              topSize={outfit.topSize}
+              bottomSize={outfit.bottomSize}
+              description={outfit.description}
+              quote={outfit.quote}
+              owner={outfit.owner}
+              
+              isFavorite={favorites[outfit.id] || false}
+              onFavoriteClick={() => handleFavorite(outfit.id)}
+            />
+          </div>
+        ))}
+      </div>
+
+      <OutfitDetailModal
+        outfit={selectedOutfit}
+        isOpen={!!selectedOutfit}
+        onClose={handleCloseModal}
+        isFavorite={
+          selectedOutfit ? favorites[selectedOutfit.id] || false : false
+        }
+        onFavoriteClick={() => {
+          if (selectedOutfit) {
+            handleFavorite(selectedOutfit.id);
+          }
+        }}
+      />
+    </>
+  );
+}
