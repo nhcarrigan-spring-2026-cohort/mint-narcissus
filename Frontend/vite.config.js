479ec0438c2edd6ac731d11310ac36877e6209ea
krutika <96387574+Krutika-Waghmare@users.noreply.github.com>
2026-02-26 14:23:27 +0530

    Revert "Fixed OutfitCard, OutfitCardModal and outfit mockData "Feat/outfit card component" (#77)"
    This reverts commit 8a13c630980499c39377a27333ddb5f3590203c8.
    
    e You are currently reverting commit 8a13c63.
    .git/COMMIT_EDITMSG [unix] (14:23 26/02/2026)  .git/COMMIT_EDITMSG [unix] (14:23 26/02/2026)  #

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
