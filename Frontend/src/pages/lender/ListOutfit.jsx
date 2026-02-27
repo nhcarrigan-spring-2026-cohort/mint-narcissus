af71d809704839866cef63faecabc6a56d03c6c0
JohannesBotha <jacobotha206@gmail.com>
2026-02-27 09:38:21 +0200

    Added OutfitAddForm (Old version) added component to pages/lender/ListOutfit.jsx

--- Frontend/src/pages/lender/ListOutfit.jsx
+++ Frontend/src/pages/lender/ListOutfit.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import { OutfitAddForm } from '../../components/outfits/OutfitAddForm';
 const ListOutfit = () => {
-  return <section className='grow'>ListOutfit</section>;
+  return <section className='grow'><OutfitAddForm /></section>;
 };
 
 export default ListOutfit;
